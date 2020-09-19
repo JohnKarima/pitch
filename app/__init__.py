4a927fd7f5dce7860de9f55a815aac7c190cc25b
John Karima <karimajohn24@gmail.com>
2020-09-19 14:06:08 +0300

    add navbar and routing for pitches folder html pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
 
 app = Flask(__name__)
 
 app.config.from_object(DevConfig)
+
+
+bootstrap = Bootstrap(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -6,6 +6,39 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Home-Pitch.com'
+    title = 'Home- Pitch.com'
     return render_template('index.html', title = title)
+
+
+@app.route('/allposts')
+def allposts():
+    '''
+    View allposts page function that returns the allposts page and its data
+    '''
+    title = 'All Posts'
+    return render_template('pitches/allposts.html', title = title)
+
+@app.route('/pitches')
+def pitches():
+    '''
+    View pitches page function that returns the pitches page and its data
+    '''
+    title = 'Pitches'
+    return render_template('pitches/pitches.html', title = title)
+
+@app.route('/pickuplines')
+def pickuplines():
+    '''
+    View pickuplines page function that returns the pickuplines page and its data
+    '''
+    title = 'Pick-up Lines'
+    return render_template('pitches/pickuplines.html', title = title)
+
+@app.route('/oneliners')
+def oneliners():
+    '''
+    View oneliners page function that returns the oneliners page and its data
+    '''
+    title = 'One-Liners'
+    return render_template('pitches/oneliners.html', title = title)
 
