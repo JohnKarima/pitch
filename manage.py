08c82309fa3f6831ede899644ea06f80ce767fbc
John Karima <karimajohn24@gmail.com>
2020-09-20 12:01:14 +0300

    import migrate, migratecommand classes

--- manage.py
+++ manage.py
@@ -4,10 +4,16 @@ from app.models import User, Role
 
 from flask_script import Manager, Server
 
+from flask_migrate import Migrate, MigrateCommand
+
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+
 manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
