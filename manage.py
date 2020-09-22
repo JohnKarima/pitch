cd37e345c332add9012f832997c2101a4a3a551d
John Karima <karimajohn24@gmail.com>
2020-09-22 07:31:52 +0300

    tweak column size in profile.html

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 
-from app.models import User, Role
+from app.models import User
 
 from flask_script import Manager, Server
 
@@ -18,7 +18,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, User = User, Role = Role)
+    return dict(app = app, db = db, User = User)
 
 if __name__ == '__main__':
     manager.run()
